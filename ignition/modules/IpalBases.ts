b616023c40409c6fa9e43cda57a82f8f4f1f637c
Axel <bidetaggle@gmail.com>
2024-05-06 15:25:19 +0100

    Update contract name

--- ignition/modules/IpalBases.ts
+++ ignition/modules/IpalBases.ts
@@ -3,7 +3,7 @@ require('@dotenvx/dotenvx').config();
 
 const IpalBasesModule = buildModule("IpalBasesModule", (m) => {
 
-  const ipalBases = m.contract("IpalBases", [
+  const ipalBases = m.contract("GatedKnowledgeManager", [
     process.env.CONTRACT_OWNER || "0x746D791F5D5853894F6888cA735528Bb19DE1912",
     "https://tbd.ltd/"
   ]);
