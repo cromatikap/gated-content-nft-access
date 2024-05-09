adb64738baf0ec7722d39f8a6454a341437eb4a4
bidetaggle <bidetaggle@gmail.com>
2024-05-09 20:14:47 +0100

    fix solhint warnings

--- contracts/erc4908/ERC4908.sol
+++ contracts/erc4908/ERC4908.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.24;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import {IERC4908} from "./IERC4908.sol";
 
 abstract contract ERC4908 is IERC4908, ERC721, ERC721Enumerable {
