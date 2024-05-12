3a6a87daf136712433742077cbfef4982711d7cb
bidetaggle <bidetaggle@gmail.com>
2024-05-12 14:05:47 +0100

    clean up GatedKnowledgeManager test file

--- test/GatedInformationManager.ts
+++ test/GatedInformationManager.ts
@@ -4,27 +4,26 @@ import hre from "hardhat";
 
 describe("Ipal Gated Knowledge Manager", function () {
   async function deployGatedKnowledgeManagerFixture() {
-    const [owner] = await hre.viem.getWalletClients();
     const baseUri = "https://uri.ltd/";
 
     const GKM = await hre.viem.deployContract("GatedKnowledgeManager", [
-      owner.account.address,
       baseUri
     ]);
 
     return {
       GKM,
-      owner,
       baseUri
     };
   }
 
   it("Should mint an NFT with the right uri", async function () {
+    /* Arrange */
+    const { GKM, baseUri } = await loadFixture(deployGatedKnowledgeManagerFixture);
+
+    /* Act */
     console.log("WIP");
 
-    // const { GKM, baseUri } = await loadFixture(deployGatedKnowledgeManagerFixture);
-
-    // await GKM.read.tokenURI([BigInt(1)])
-    // // expect(await GKM.read.tokenURI([BigInt(10)])).to.equal(baseUri + "10.json");
+    /* Assert */
+    expect(true).to.equal(true);
   });
 });
